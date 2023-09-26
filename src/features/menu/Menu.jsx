b5357f1d127666b525019c77226a2c28ecebdd95
Paulo Amorim <phdja25@gmail.com>
2023-09-26 17:03:25 -0300

    build fetch on loading for menu.

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,22 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../service/apiRestaurant";
+import MenuItem from "../menu/MenuItem";
+
 function Menu() {
-  return <h1>Menu</h1>;
+  const data = useLoaderData();
+  return (
+    <h1>
+      <ul>
+        {data.map((pizza) => (
+          <MenuItem pizza={pizza} key={pizza.id} />
+        ))}
+      </ul>
+    </h1>
+  );
 }
 
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
+}
 export default Menu;
